684295f4bc63c62a579136b9f6789daee82a6350
Maera <baeitsmae@gmail.com>
2022-05-04 21:53:59 +0200

    Résolution de beug dans la page NewBill.js

--- Billed-app-FR-Front/src/containers/NewBill.js
+++ Billed-app-FR-Front/src/containers/NewBill.js
@@ -6,43 +6,59 @@ export default class NewBill {
     this.document = document
     this.onNavigate = onNavigate
     this.store = store
+
     const formNewBill = this.document.querySelector(`form[data-testid="form-new-bill"]`)
     formNewBill.addEventListener("submit", this.handleSubmit)
+
     const file = this.document.querySelector(`input[data-testid="file"]`)
     file.addEventListener("change", this.handleChangeFile)
+
     this.fileUrl = null
     this.fileName = null
     this.billId = null
+
     new Logout({ document, localStorage, onNavigate })
   }
+
   handleChangeFile = e => {
     e.preventDefault()
+
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
-    const filePath = e.target.value.split(/\\/g)
-    const fileName = filePath[filePath.length-1]
+    const fileName = file.name
     const formData = new FormData()
     const email = JSON.parse(localStorage.getItem("user")).email
+
     formData.append('file', file)
     formData.append('email', email)
 
-    this.store
-      .bills()
-      .create({
-        data: formData,
-        headers: {
-          noContentType: true
-        }
-      })
-      .then(({fileUrl, key}) => {
-        console.log(fileUrl)
-        this.billId = key
-        this.fileUrl = fileUrl
-        this.fileName = fileName
-      }).catch(error => console.error(error))
+    // Ajout d'une condition pour vérifier que les fichiers ne soient qu'au format png, jpg ou jpeg
+    if (fileName.endsWith('.png') || fileName.endsWith('jpeg') || fileName.endsWith('jpg')) {
+      this.store
+        .bills()
+        .create({
+          data: formData,
+          headers: {
+            noContentType: true
+          }
+        })
+        .then(({fileUrl, key}) => {
+          console.log(fileUrl)
+          this.billId = key
+          this.fileUrl = fileUrl
+          this.fileName = fileName
+        }).catch(error => console.error(error))
+      } 
+      else { // sinon un message d'erreur apparait
+        e.target.value = null
+        alert("Votre fichier doit être au format PNG, JPEG ou JPG.")
+      }
   }
+
   handleSubmit = e => {
     e.preventDefault()
+
     console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
+
     const email = JSON.parse(localStorage.getItem("user")).email
     const bill = {
       email,
@@ -57,6 +73,7 @@ export default class NewBill {
       fileName: this.fileName,
       status: 'pending'
     }
+
     this.updateBill(bill)
     this.onNavigate(ROUTES_PATH['Bills'])
   }
